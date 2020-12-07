57bea54f6a37d9b14838e2df6e3663c37bc41513
huchong <1178869226@qq.com>
2020-12-07 18:35:58 -0500

    [REVISE] schedule handler to extract attraction info

--- travel-planner-backend/handlers/proj_schedule_handler.py
+++ travel-planner-backend/handlers/proj_schedule_handler.py
@@ -1,5 +1,8 @@
 import json
 import logging
+from decimal import Decimal
+from functools import reduce
+from operator import iconcat
 
 import boto3
 
@@ -8,6 +11,13 @@ logger.setLevel(logging.DEBUG)
 
 user_table = boto3.resource('dynamodb').Table('userTable')
 schedule_table = boto3.resource('dynamodb').Table('scheduleTable')
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, Decimal):
+            return float(o)
+        return super(DecimalEncoder, self).default(o)
 
 
 def get_target_user(user_id):
@@ -47,7 +57,7 @@ def get_target_schedule(schedule_id):
         )
 
         if "Item" in response and len(response["Item"]) != 0:
-            logger.debug(json.dumps(response, indent=2))
+            logger.debug(json.dumps(response, indent=2, cls=DecimalEncoder))
             return True, response
         return False, {
             'statusCode': 400,
@@ -109,6 +119,78 @@ def delete_target_schedule(schedule_id):
         }
 
 
+def extract_item(item):
+    bucket = item["bucket"]
+    object_key = item["objectKey"]
+    return "https://{:s}.s3.amazonaws.com/{:s}".format(bucket, object_key)
+
+
+def extract_attraction(attraction_info):
+    attraction_img_urls = list(map(extract_item, attraction_info["attractionImgs"]))
+    del attraction_info["attractionImgs"]
+    attraction_info["attractionImgUrls"] = attraction_img_urls
+    return attraction_info
+
+
+def batch_get_attraction(attraction_id_list):
+    try:
+        response = boto3.resource('dynamodb').batch_get_item(
+            RequestItems={
+                "attractionTable": {
+                    "Keys": list(map(lambda attraction_id: {"attractionId": attraction_id}, attraction_id_list)),
+                }
+            }
+        )
+
+        if "Responses" in response and len(response["UnprocessedKeys"]) == 0:
+            return True, response["Responses"]
+        return False, {
+            'statusCode': 400,
+            'body': json.dumps({
+                "code": 400,
+                "msg": "can not get attraction attributes"
+            })
+        }
+    except Exception as e:
+        logger.error(e)
+        return False, {
+            'statusCode': 400,
+            'body': json.dumps({
+                "code": 400,
+                "msg": "attraction id doesn't exist"
+            })
+        }
+
+
+def get_preselect_schedule(target_schedule):
+    attraction_id_list = list(target_schedule["scheduleContent"].keys())
+    succ, response = batch_get_attraction(set(attraction_id_list))
+    if not succ:
+        return False, response
+    attraction_info_list = response["attractionTable"]
+    attraction_extracted_info_list = list(map(extract_attraction, attraction_info_list))
+    for info in attraction_extracted_info_list:
+        target_schedule["scheduleContent"][info["attractionId"]].update(info)
+    return True, target_schedule
+
+
+def get_complete_schedule(target_schedule):
+    attraction_id_list = reduce(iconcat, target_schedule["scheduleContent"]["dayScheduleContents"])
+    succ, response = batch_get_attraction(set(attraction_id_list))
+    if not succ:
+        return False, response
+    attraction_info_list = response["attractionTable"]
+    attraction_extracted_info_list = list(map(extract_attraction, attraction_info_list))
+    attraction_info_map = dict(zip(list(map(lambda attraction_info: attraction_info["attractionId"],
+                                            attraction_extracted_info_list)),
+                                   attraction_info_list))
+    for ind, day_schedule_content in enumerate(target_schedule["scheduleContent"]["dayScheduleContents"]):
+        target_schedule["scheduleContent"]["dayScheduleContents"][ind] = list(map(lambda attraction_id:
+                                                                                  attraction_info_map[attraction_id],
+                                                                                  day_schedule_content))
+    return True, target_schedule
+
+
 def get_schedule(user_id, schedule_id):
     succ, response = get_target_schedule(schedule_id)
     if not succ:
@@ -122,9 +204,17 @@ def get_schedule(user_id, schedule_id):
                 "msg": "Permission Denied!"
             })
         }
+    if target_schedule["scheduleType"].upper() == "PRESELECT":
+        succ, response = get_preselect_schedule(target_schedule)
+        if not succ:
+            return response
+    else:
+        succ, response = get_complete_schedule(target_schedule)
+        if not succ:
+            return response
     return {
         'statusCode': 200,
-        'body': json.dumps(target_schedule)
+        'body': json.dumps(target_schedule, cls=DecimalEncoder)
     }
 
 
