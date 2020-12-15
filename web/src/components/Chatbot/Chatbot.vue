1dd7533fcf41bcb79b2c7263c1d01fd3fd1ec4b5
JACKCHEN96 <cchenwenjie0901@gmail.com>
2020-12-15 13:42:04 +0800

    small update

--- web/src/components/Chatbot/Chatbot.vue
+++ web/src/components/Chatbot/Chatbot.vue
@@ -4,43 +4,43 @@
   </el-scrollbar>
 </template>
 <script>
-  import { AmplifyEventBus } from "aws-amplify-vue";
-  // import { Auth } from "aws-amplify";
-  import { Interactions } from "aws-amplify";
-  export default {
-    name: "ChatbotComponent",
-    data() {
-      return {
-        chatbotConfig: {
-          bot: "ScheduleBot_first_dev",
-          clearComplete: false,
-          botTitle: "Helper",
-        },
-      };
-    },
-    methods: {
-      initChatbot() {
-        Interactions.onComplete("ScheduleBot_first_dev", this.handleComplete);
+import { AmplifyEventBus } from "aws-amplify-vue";
+// import { Auth } from "aws-amplify";
+import { Interactions } from "aws-amplify";
+export default {
+  name: "ChatbotComponent",
+  data() {
+    return {
+      chatbotConfig: {
+        bot: "ScheduleBot_first_dev",
+        clearComplete: true,
+        botTitle: "Helper",
       },
-      handleComplete(err, confirmation) {
-        console.log("ppprint", JSON.stringify(confirmation));
-        if (err) {
-          alert(err);
-          return;
-        }
-        alert(JSON.stringify(confirmation));
-        console.log("chatComplete", confirmation);
-        AmplifyEventBus.$emit("chatComplete", this.options.botTitle);
-      },
+    };
+  },
+  methods: {
+    initChatbot() {
+      Interactions.onComplete("ScheduleBot_first_dev", this.handleComplete);
     },
-    mounted() {
-      this.initChatbot();
+    handleComplete(err, confirmation) {
+      console.log("ppprint", JSON.stringify(confirmation));
+      if (err) {
+        alert(err);
+        return;
+      }
+      // alert(JSON.stringify(confirmation));
+      console.log("chatComplete", confirmation);
+      AmplifyEventBus.$emit("chatComplete", this.options.botTitle);
     },
-  };
+  },
+  mounted() {
+    this.initChatbot();
+  },
+};
 </script>
 <style scoped>
-  .chatbot {
-    min-height: 100px;
-    max-height: 200px;
-  }
+.chatbot {
+  min-height: 100px;
+  max-height: 200px;
+}
 </style>
